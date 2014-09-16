6cdc34445c89b51182fa374422bd54471a4373df
Bliebson <brian.liebson@students.olin.edu>
2014-09-16 14:05:11 -0400

    added 4x4 option

--- grid.py
+++ grid.py
@@ -4,6 +4,7 @@ Created on Tue Sep 16 10:44:59 2014
 
 @author: bliebson
 """
+a = int(raw_input('Size of Grid'))
 
 def two(f):
     f()
@@ -19,20 +20,41 @@ def print_h_part():
 def print_v_part():
     print '|    ',
     
-def print_h_full():
+def print_h_full2():
     two(print_h_part)
     print '+'
     
-def print_v_full():
+def print_h_full4():
+    four(print_h_part)
+    print '+'
+    
+def print_v_full2():
     two(print_v_part)
     print '|'
     
-def print_row():
-    print_h_full()
-    four(print_v_full)
+def print_v_full4():
+    four(print_v_part)
+    print '|'
     
-def print_grid():
-    two(print_row)
-    print_h_full()
+def print_row2():
+    print_h_full2()
+    four(print_v_full2)
     
-print_grid()
+def print_row4():
+    print_h_full4()
+    four(print_v_full4)
+    
+def print_grid2():
+    two(print_row2)
+    print_h_full2()
+    
+def print_grid4():
+    four(print_row4)
+    print_h_full4()
+    
+if a == 2:
+    print_grid2()
+    
+if a == 4:
+    print_grid4()
+    
